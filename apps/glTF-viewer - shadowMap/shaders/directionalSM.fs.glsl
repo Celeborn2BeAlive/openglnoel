f032ac8bbb6317017e79e1726cb810e405ff53ed
Amel <amel.negra@gmail.com>
2019-03-31 21:13:22 +0200

    delete unused app

--- apps/glTF-viewer - shadowMap/shaders/directionalSM.fs.glsl
+++
@@ -1,6 +0,0 @@
-#version 330 core
-out vec3 fColor;
-void main()
-{
-    fColor = vec3(0);
-}
--- apps/glTF-viewer - shadowMap/shaders/directionalSM.vs.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-layout(location = 0) in vec3 aPosition;
-uniform mat4 uDirLightViewProjMatrix;
-
-void main()
-{
-    gl_Position =  uDirLightViewProjMatrix * vec4(aPosition, 1);
-}
